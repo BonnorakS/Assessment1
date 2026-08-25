56167b1909651fd9a728631d56138a07e8d0251a
BonnorakS <a3150511@adelaide.edu.au>
2026-08-25 12:36:29 +0930

    Initial Spring Boot project

---
+++ src/test/java/au/edu/adelaide/Assessment1/Assessment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package au.edu.adelaide.Assessment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assessment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
